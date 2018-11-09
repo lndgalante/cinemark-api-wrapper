e87402e9207449c1155254bbde62b8d194c36990
lndgalante <lndgalante@gmail.com>
2018-11-09 20:34:03 -0300

    Update API

--- index.js
+++ index.js
@@ -1,16 +1,126 @@
 const express = require('express')
 const cors = require('cors')({ origin: true })
 const apicache = require('apicache')
+const safeParse = require('safe-json-parse/tuple')
+
 const fetch = require('node-fetch')
+const dayjs = require('dayjs')
+
+require('dayjs/locale/es')
+dayjs.locale('es')
+
+const { toTitleCase, fixName, getImdbInfo } = require('./utils')
 
 const app = express()
 const cache = apicache.middleware
 
 app.use(cors)
 
-app.get('*', cache('6 hours'), async (req, res) => {
-  const response = await fetch('https://cinemark-220917.appspot.com')
-  const data = await response.json()
+app.get('/cinemas', cache('24 hours'), async (req, res) => {
+  const response = await fetch('https://www.cinemarkhoyts.com.ar/billboard.ashx')
+  const code = await response.text()
+  const [err, json] = safeParse(code.slice(15, -1))
+
+  const data = json.Cinemas.map(({ Id, Name, Address, Features, decLatitude, decLongitude, URLGoogleMaps }) => ({
+    cinemaId: Id,
+    value: Name,
+    label: fixName(Name),
+    features: Features,
+    latitude: decLatitude,
+    longitude: decLongitude,
+    tags: [{ tag: Address, link: URLGoogleMaps }, { tag: Features, link: 'https://www.cinemarkhoyts.com.ar/formatos' }],
+  }))
+
+  res.send(data)
+})
+
+app.get('/movies', cache('3 hours'), async (req, res) => {
+  const response = await fetch('https://www.cinemarkhoyts.com.ar/billboard.ashx')
+  const code = await response.text()
+  const [err, json] = safeParse(code.slice(15, -1))
+
+  const premieres = json.Films.filter(
+    ({ AttributeList: attributes }) => attributes.includes(0) && !attributes.includes(3)
+  )
+
+  const data = await Promise.all(
+    premieres.map(
+      async ({
+        Id,
+        Name,
+        Rating,
+        Description,
+        Duration,
+        Category,
+        URLPoster,
+        URLTrailerYoutube,
+        PersonList,
+        CinemaList,
+      }) => {
+        const data = {
+          movieId: Id,
+          name: toTitleCase(Name),
+          minAge: Rating.split(' ')[0],
+          description: Description,
+          duration: `${Duration} minutos`,
+          category: Category,
+          poster: URLPoster,
+          youTubeId: URLTrailerYoutube.split('.be/')[1],
+          inCinemas: CinemaList,
+          cast: PersonList.reduce(
+            (cast, { Type, Name }) => {
+              if (Type === 'D') {
+                cast.directors.push(Name)
+                return cast
+              }
+              if (Type === 'A') {
+                cast.actors.push(Name)
+                return cast
+              }
+            },
+            { directors: [], actors: [] }
+          ),
+        }
+
+        try {
+          const imdbInfo = await getImdbInfo(Name)
+          return { ...data, ...imdbInfo }
+        } catch (error) {
+          return data
+        }
+      }
+    )
+  )
+
+  res.send(data)
+})
+
+app.get('/movie', cache('15 minutes'), async (req, res) => {
+  const response = await fetch('https://www.cinemarkhoyts.com.ar/billboard.ashx')
+  const code = await response.text()
+  const [err, json] = safeParse(code.slice(15, -1))
+
+  const { movieId, cinemaId } = req.query
+
+  const data = json.Films.filter(({ Id }) => Id === movieId).map(({ Name, MovieList }) => ({
+    name: toTitleCase(Name),
+    shows: MovieList.filter(({ CinemaList }) => CinemaList.some(({ Id }) => Id === Number(cinemaId))).map(
+      ({ Format, Version, CinemaList }) => ({
+        format: toTitleCase(Format)
+          .split(' ')
+          .map((value, index) => (index === 0 ? value.toUpperCase() : value))
+          .join(' '),
+        version: toTitleCase(Version),
+        cinemas: CinemaList.filter(({ Id }) => Id === Number(cinemaId)).map(({ Id: cinemaId, SessionList }) =>
+          SessionList.map(({ Id: sessionId, Feature: featureId, Dtm }) => ({
+            date: dayjs(Dtm).format('dddd D [de] MMMM'),
+            time: dayjs(Dtm).format('HH[:]mm'),
+            link: `https://tickets.cinemarkhoyts.com.ar/NSTicketing/?CinemaId=${cinemaId}&SessionId=${sessionId}&FeatureId=${featureId}`,
+          }))
+        )[0],
+      })
+    ),
+  }))[0]
 
   res.send(data)
 })
---
+++ utils/index.js
@@ -0,0 +1,44 @@
+const capitalize = require('lodash.capitalize')
+
+const toTitleCase = string =>
+  string
+    .split(' ')
+    .map(capitalize)
+    .join(' ')
+
+const fixName = name => {
+  if (name === 'Hoyts Nuevocentro') return 'Hoyts Nuevo Centro'
+  if (name === 'Cinemark Tortugas') return 'Cinemark Tortuguitas'
+  if (name === 'Hoyts Moron') return 'Hoyts Morón'
+  if (name === 'Cinemark Neuquen') return 'Cinemark Neuquén'
+
+  return name
+}
+
+const getImdbInfo = async title => {
+  const currentYear = new Date().getFullYear()
+
+  const apiKey = '59d3174235953c532caf53f23c2d4c30'
+  const baseUrl = 'https://api.themoviedb.org/3/search/movie'
+  const options = `&page=1&include_adult=false&year=${currentYear}`
+
+  const endpoint = `${baseUrl}?api_key=${apiKey}&query=${encodeURI(title)}${options}`
+
+  const res = await fetch(endpoint)
+  const data = await res.json()
+
+  if (data.total_results === 0 || data.total_results > 1) return
+
+  const [movie] = data.results
+
+  const { vote_average: votes, poster_path: posterPath } = movie
+
+  const baseImageUrl = 'https://image.tmdb.org/t/p'
+  const withWidth = width => `w${width}`
+
+  const poster = `${baseImageUrl}/${withWidth(300)}/${posterPath}`
+
+  return { votes, poster }
+}
+
+module.exports = { toTitleCase, fixName, getImdbInfo }
